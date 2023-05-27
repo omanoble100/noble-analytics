6c5e67d08d4d1b4ca29a4eae7c6d84910903c2a1
omanoble100 <amosoma100@gmail.com>
2023-05-27 11:24:16 +0100

    --commit: analytics

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Laptop from '../assets/laptop.jpg'
+
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+            <img className="w-[500px] mx-auto my-4" src={Laptop} alt="/" />
+
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARD</p>
+                <h1 className='font-bold lg:text-4xl sm:text-3xl text-2xl py-2'>Manage Data Analytics Centrally</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis atque odio, explicabo repellendus recusandae quia quaerat exercitationem cumque ipsa et, minima eius, debitis aperiam obcaecati tempore aliquam! Voluptate, possimus sequi?</p>
+                <button className='text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 bg-black'>Get Started</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Analytics
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>NOBLE.</h1>
+
+        <ul className='hidden md:flex'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>Company</li>
+            <li className='p-4'>Resources</li>
+            <li className='p-4'>About</li>
+            <li className='p-4'>Contact</li>
+        </ul>
+
+        <div className='block md:hidden' onClick={() => setToggle(!toggle)}>
+            {toggle ? <AiOutlineClose size={25}  /> : <AiOutlineMenu size={25}/>}
+        </div>
+
+       
+          <div className={toggle ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out duration-700'}>
+            
+          <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>NOBLE.</h1>
+            
+            <ul className='uppercase p-4'>
+                <li className='p-4 border-b border-gray-600'>Home</li>
+                <li className='p-4 border-b border-gray-600'>Company</li>
+                <li className='p-4 border-b border-gray-600'>Resources</li>
+                <li className='p-4 border-b border-gray-600'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
